cc29447480d51671f7675d83a692371b25c5362f
NicolasD1440 <82782749+NicolasD1440@users.noreply.github.com>
2021-05-23 11:57:17 -0500

    Update Inicio.java

--- SpotyFox/src/inicio/Inicio.java
+++ SpotyFox/src/inicio/Inicio.java
@@ -20,11 +20,13 @@ public class Inicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		metod.addElement(new Canciones("/Caratula1.jpg","Another one bites the dust","Queen","recursos/Queen-Another.mp3", 3.74));
-		metod.addElement(new Canciones("/Caratula2.jpg","Dile","Don Omar","recursos/Dile.mp3", 3.5 ));
-		metod.addElement(new Canciones("/Caratula3.jpg","Cuando hablan las miradas","Guayacan","recursos/Cuando.mp3", 4.37));
-		metod.addElement(new Canciones("/Caratula4.jpg","Hotaru No Hikari","Ikimono-Gakari","recursos/naruto.mp3", 4.1));
-		
+		metod.addElement(new Canciones("/Caratula1.jpg","Another one bites the dust","Queen","recursos/Queen-Another.mp3", "3:42"));
+		metod.addElement(new Canciones("/Caratula2.jpg","Dile","Don Omar","recursos/Dile.mp3", "3:26"));
+		metod.addElement(new Canciones("/Caratula3.jpg","Cuando hablan las miradas","Guayacan","recursos/Cuando.mp3", "4:21"));
+		metod.addElement(new Canciones("/Caratula4.jpg","Hotaru No Hikari","Ikimono-Gakari","recursos/naruto.mp3", "4:01"));
+		metod.addElement(new Canciones("/Caratula5.jpg","A Kind of Magic","Queen","recursos/kind.mp3", "4:43"));
+		metod.addElement(new Canciones("/Caratula6.jpg","The Final Countdown","Europe","recursos/Final.mp3", "4:56"));
+		metod.addElement(new Canciones("/Caratula7.jpg","Inferno","Mrs. GREEN APPLE ","recursos/Fire.mp3", "3:31"));
 		Portada ventana = new Portada();
 		ventana.setVisible(true);
 	}
